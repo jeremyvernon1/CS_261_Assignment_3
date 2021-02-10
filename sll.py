cd89fbdacec80d98499c84cc180e3beb5b5fe74a
jeremyvernon1 <jeremyvernon@icloud.com>
2021-02-09 21:02:12 -0500

    add_front working

--- sll.py
+++ sll.py
@@ -1,7 +1,7 @@
 # Course: CS261 - Data Structures
-# Student Name:
-# Assignment:
-# Description:
+# Student Name: Jeremy Vernon
+# Assignment: 3
+# Description: Singly Linked List
 
 
 
@@ -77,9 +77,11 @@ class LinkedList:
 
     def add_front(self, value: object) -> None:
         """
-        TODO: Write this implementation
+        Adds an element to the beginning of the list
         """
-        pass
+        front_node = SLNode(value)
+        front_node.next = self.head.next
+        self.head.next = front_node
 
     def add_back(self, value: object) -> None:
         """
@@ -147,129 +149,127 @@ class LinkedList:
 
 
 if __name__ == '__main__':
-    pass
+    print('\n add_front example 1')
+    list = LinkedList()
+    print(list)
+    list.add_front('A')
+    list.add_front('B')
+    list.add_front('C')
+    print(list)
 
-    # print('\n# add_front example 1')
-    # list = LinkedList()
-    # print(list)
-    # list.add_front('A')
-    # list.add_front('B')
-    # list.add_front('C')
-    # print(list)
-    #
-    #
-    # print('\n# add_back example 1')
-    # list = LinkedList()
-    # print(list)
-    # list.add_back('C')
-    # list.add_back('B')
-    # list.add_back('A')
-    # print(list)
-    #
-    #
-    # print('\n# insert_at_index example 1')
-    # list = LinkedList()
-    # test_cases = [(0, 'A'), (0, 'B'), (1, 'C'), (3, 'D'), (-1, 'E'), (5, 'F')]
-    # for index, value in test_cases:
-    #     print('Insert of', value, 'at', index, ': ', end='')
-    #     try:
-    #         list.insert_at_index(index, value)
-    #         print(list)
-    #     except Exception as e:
-    #         print(type(e))
-    #
-    #
-    # print('\n# remove_front example 1')
-    # list = LinkedList([1, 2])
-    # print(list)
-    # for i in range(3):
-    #     try:
-    #         list.remove_front()
-    #         print('Successful removal', list)
-    #     except Exception as e:
-    #         print(type(e))
-    #
-    #
-    # print('\n# remove_back example 1')
-    # list = LinkedList()
-    # try:
-    #     list.remove_back()
-    # except Exception as e:
-    #     print(type(e))
-    # list.add_front('Z')
-    # list.remove_back()
-    # print(list)
-    # list.add_front('Y')
-    # list.add_back('Z')
-    # list.add_front('X')
-    # print(list)
-    # list.remove_back()
-    # print(list)
-    #
-    #
-    # print('\n# remove_at_index example 1')
-    # list = LinkedList([1, 2, 3, 4, 5, 6])
-    # print(list)
-    # for index in [0, 0, 0, 2, 2, -2]:
-    #     print('Removed at index:', index, ': ', end='')
-    #     try:
-    #         list.remove_at_index(index)
-    #         print(list)
-    #     except Exception as e:
-    #         print(type(e))
-    # print(list)
-    #
-    #
-    # print('\n# get_front example 1')
-    # list = LinkedList(['A', 'B'])
-    # print(list.get_front())
-    # print(list.get_front())
-    # list.remove_front()
-    # print(list.get_front())
-    # list.remove_back()
-    # try:
-    #     print(list.get_front())
-    # except Exception as e:
-    #     print(type(e))
-    #
-    #
-    # print('\n# get_back example 1')
-    # list = LinkedList([1, 2, 3])
-    # list.add_back(4)
-    # print(list.get_back())
-    # list.remove_back()
-    # print(list)
-    # print(list.get_back())
-    #
-    #
-    # print('\n# remove example 1')
-    # list = LinkedList([1, 2, 3, 1, 2, 3, 1, 2, 3])
-    # print(list)
-    # for value in [7, 3, 3, 3, 3]:
-    #     print(list.remove(value), list.length(), list)
-    #
-    #
-    # print('\n# count example 1')
-    # list = LinkedList([1, 2, 3, 1, 2, 2])
-    # print(list, list.count(1), list.count(2), list.count(3), list.count(4))
-    #
-    #
-    # print('\n# slice example 1')
-    # list = LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    # ll_slice = list.slice(1, 3)
-    # print(list, ll_slice, sep="\n")
-    # ll_slice.remove_at_index(0)
-    # print(list, ll_slice, sep="\n")
-    #
-    #
-    # print('\n# slice example 2')
-    # list = LinkedList([10, 11, 12, 13, 14, 15, 16])
-    # print("SOURCE:", list)
-    # slices = [(0, 7), (-1, 7), (0, 8), (2, 3), (5, 0), (5, 3), (6, 1)]
-    # for index, size in slices:
-    #     print("Slice", index, "/", size, end="")
-    #     try:
-    #         print(" --- OK: ", list.slice(index, size))
-    #     except:
-    #         print(" --- exception occurred.")
+
+    print('\n add_back example 1')
+    list = LinkedList()
+    print(list)
+    list.add_back('C')
+    list.add_back('B')
+    list.add_back('A')
+    print(list)
+
+
+    print('\n insert_at_index example 1')
+    list = LinkedList()
+    test_cases = [(0, 'A'), (0, 'B'), (1, 'C'), (3, 'D'), (-1, 'E'), (5, 'F')]
+    for index, value in test_cases:
+     print('Insert of', value, 'at', index, ': ', end='')
+     try:
+         list.insert_at_index(index, value)
+         print(list)
+     except Exception as e:
+         print(type(e))
+
+
+    print('\n remove_front example 1')
+    list = LinkedList([1, 2])
+    print(list)
+    for i in range(3):
+     try:
+         list.remove_front()
+         print('Successful removal', list)
+     except Exception as e:
+         print(type(e))
+
+
+    print('\n remove_back example 1')
+    list = LinkedList()
+    try:
+     list.remove_back()
+    except Exception as e:
+     print(type(e))
+    list.add_front('Z')
+    list.remove_back()
+    print(list)
+    list.add_front('Y')
+    list.add_back('Z')
+    list.add_front('X')
+    print(list)
+    list.remove_back()
+    print(list)
+
+
+    print('\n remove_at_index example 1')
+    list = LinkedList([1, 2, 3, 4, 5, 6])
+    print(list)
+    for index in [0, 0, 0, 2, 2, -2]:
+     print('Removed at index:', index, ': ', end='')
+     try:
+         list.remove_at_index(index)
+         print(list)
+     except Exception as e:
+         print(type(e))
+    print(list)
+
+
+    print('\n get_front example 1')
+    list = LinkedList(['A', 'B'])
+    print(list.get_front())
+    print(list.get_front())
+    list.remove_front()
+    print(list.get_front())
+    list.remove_back()
+    try:
+     print(list.get_front())
+    except Exception as e:
+     print(type(e))
+
+
+    print('\n get_back example 1')
+    list = LinkedList([1, 2, 3])
+    list.add_back(4)
+    print(list.get_back())
+    list.remove_back()
+    print(list)
+    print(list.get_back())
+
+
+    print('\n remove example 1')
+    list = LinkedList([1, 2, 3, 1, 2, 3, 1, 2, 3])
+    print(list)
+    for value in [7, 3, 3, 3, 3]:
+     print(list.remove(value), list.length(), list)
+
+
+    print('\n count example 1')
+    list = LinkedList([1, 2, 3, 1, 2, 2])
+    print(list, list.count(1), list.count(2), list.count(3), list.count(4))
+
+
+    print('\n slice example 1')
+    list = LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    ll_slice = list.slice(1, 3)
+    print(list, ll_slice, sep="\n")
+    ll_slice.remove_at_index(0)
+    print(list, ll_slice, sep="\n")
+
+
+    print('\n slice example 2')
+    list = LinkedList([10, 11, 12, 13, 14, 15, 16])
+    print("SOURCE:", list)
+    slices = [(0, 7), (-1, 7), (0, 8), (2, 3), (5, 0), (5, 3), (6, 1)]
+    for index, size in slices:
+     print("Slice", index, "/", size, end="")
+     try:
+         print(" --- OK: ", list.slice(index, size))
+     except:
+         print(" --- exception occurred.")
 
